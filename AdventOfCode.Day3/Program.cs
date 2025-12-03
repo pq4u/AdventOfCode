5b2d8b79b83f596672549237f798e74cbc8366b9
pq4u <paweltes19@gmail.com>
2025-12-03 21:58:09 +0100

    3rd day

---
+++ AdventOfCode.Day3/Program.cs
@@ -0,0 +1,53 @@
+﻿var lines =  File.ReadAllLines("C:\\repos\\AoC\\AdventOfCode\\AdventOfCode.Day3\\input.txt");
+long totalOutput = 0;
+
+foreach (var line in lines)
+{
+    var joltage = new int[12];
+    var left = line.Length;
+    var toTake = 12;
+
+    var listToWorkWith = line.Select(c => c - '0').ToList();
+    var numbersTaken = 0;
+    var currentIndexToFill = 0;
+    
+    for (int j = 0; j < joltage.Length; j++)
+    {
+        currentIndexToFill = j;
+
+        for (int i = 0; i < listToWorkWith.Count; i++)
+        {
+            var num = int.Parse(listToWorkWith[i].ToString());
+
+            if (toTake > left)
+            {
+                listToWorkWith = listToWorkWith.Skip(numbersTaken + 1).ToList();
+                break;
+            }
+
+            if (num > joltage[currentIndexToFill])
+            {
+                joltage[currentIndexToFill] = num;
+                numbersTaken = i;
+            }
+        
+            left--;
+        }
+        left = listToWorkWith.Count();
+        numbersTaken = 0;
+        toTake--;
+    }
+
+    // very memory friendly way of converting array to string
+    var joltageString = "";
+    
+    for (int i = 0; i < joltage.Length; i++)
+    {
+        joltageString += joltage[i];
+    }
+
+    Console.WriteLine(joltageString);
+    totalOutput += long.Parse(joltageString);
+}
+
+Console.WriteLine($"Total: {totalOutput}");
